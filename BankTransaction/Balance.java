87263816758cadef41778326d37ba9438dc884ef
Nagasiva Vakadani <135602747+Nagasiva-2506@users.noreply.github.com>
2023-09-22 19:18:09 +0530

    Delete BankTransaction directory

--- BankTransaction/Balance.java
+++
@@ -1,7 +0,0 @@
-package p1;
-public class Balance {
-	public float bal=2000.0F;
-	public float getBalance() {
-		return bal; 
-	}
-}
--- BankTransaction/Transaction.java
+++
@@ -1,5 +0,0 @@
-package p1;
-public interface Transaction {
-	public Balance b = new Balance();
-	public abstract void process(int amt);
-}
